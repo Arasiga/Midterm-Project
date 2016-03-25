1dd8dc0d48b277a585ffc98ec5d4065e3b69197c
hjs <h@s.e>
2016-03-25 18:18:19 -0400

    user, project, membership models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,39 @@
-
-
 class Project < ActiveRecord::Base
 
-  has_and_belongs_to_many :users
+  has_many :memberships
+  has_many :users, :through => :memberships
 
+  validates_presence_of :name
+
+  class InvalidUser < TypeError
+    def initialize(msg="Invalid user")
+    end
+  end
+
+  def set_admin(user)
+    # binding.pry
+    raise InvalidUser.new if !user.is_a?(User)
+    raise InvalidUser.new("User is not in project") if !self.users.include?(user)
+    required_membership = nil
+    self.memberships.each do |member|
+     required_membership = member if member.user == user
+     member.update!(is_admin: false)
+    end
+    required_membership.update!(is_admin: true)
+  end
+
+  def admin
+    self.memberships.reduce(nil) do |administrator, member|
+      administrator = member.user if member.is_admin
+      administrator
+    end
+  end
+
+
+  def add(new_user)
+    raise InvalidUser.new if !new_user.is_a?(User)
+    raise InvalidUser.new("User is already in project") if self.users.include?(new_user)
+    m = Membership.new(user: new_user, project: self, is_admin: false)
+    m.save
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@
 
 class User < ActiveRecord::Base
 
-  has_many :projects
+  has_many :memberships
+  has_many :projects, :through => :memberships
 
   validates_presence_of :first_name
   validates_presence_of :last_name
